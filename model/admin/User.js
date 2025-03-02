3ddcbf9f585e753c5902d2079f9c7aed99787538
Your Name <pallokesh495@gmail.com>
2025-03-02 09:33:24 +0530

    1.2.0 affiliate added

--- model/admin/User.js
+++ model/admin/User.js
@@ -2,6 +2,7 @@ import { DataTypes } from 'sequelize';
 import sequelize from '../../config/db.js';
 import bcrypt from 'bcryptjs';
 import Wallet from './Wallet.js'; // Import Wallet model
+import Affiliate from './Affiliate.js'; // Import Affiliate model
 
 const User = sequelize.define('User', {
     user_id: {
@@ -55,8 +56,27 @@ const User = sequelize.define('User', {
         afterCreate: async (user) => {
             // Automatically create a wallet for the user
             await Wallet.create({ user_id: user.user_id, balance: 0.0, coin_type: user.coin_type });
+
+            // Generate a unique referral code and link
+            const referralCode = generateReferralCode(user.user_id);
+            const referralLink = `http://yourapp.com/signup?ref=${referralCode}`;
+
+            // Create an affiliate entry for the new user
+            await Affiliate.create({
+                uid: user.user_id,
+                name: user.username,
+                referral_link: referralLink,
+                referral_code: referralCode,
+                friends: 0, // Initialize friends count to 0
+            });
         },
     },
 });
+
+// Function to generate a unique referral code
+const generateReferralCode = (userId) => {
+    const code = `REF${userId}${Math.random().toString(36).substring(2, 8).toUpperCase()}`;
+    return code;
+};
 
 export default User;
